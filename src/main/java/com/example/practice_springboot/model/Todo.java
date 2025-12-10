8eed31e45512218d16912f04d2664af12118dfe5
shouras <h.tomohiro1219@icloud.com>
2025-12-10 16:40:05 +0900

    add taskArray
    タスクの配列に格納できるようにしたよ

--- src/main/java/com/example/practice_springboot/model/Todo.java
+++ src/main/java/com/example/practice_springboot/model/Todo.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Todo{
 	public static void main(String[] args){
 		System.out.println("Todoリストを作成します！");
+
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<String> todoList = new ArrayList<>();
+			
+		
+		while(true){
+			System.out.print("タスクを入力してください：");
+			String task = scanner.nextLine();
+
+			System.out.println("「" + task + "」を登録しました");
+			if(task.equals("exit")){
+				break;
+			}
+			todoList.add(task);
+			System.out.println("現在のタスク：" + todoList);
+
+		}
+	System.out.println("アプリを終了します。");
+
 	}
 }
 
