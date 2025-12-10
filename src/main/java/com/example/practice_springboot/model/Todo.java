3b86b27a333e7e96405a25adfa05a1a6bf0250bd
shouras <h.tomohiro1219@icloud.com>
2025-12-11 01:10:47 +0900

    add task_delete
    削除機能を追加

--- src/main/java/com/example/practice_springboot/model/Todo.java
+++ src/main/java/com/example/practice_springboot/model/Todo.java
@@ -3,26 +3,44 @@ import java.util.ArrayList;
 
 public class Todo{
 	public static void main(String[] args){
-		System.out.println("Todoリストを作成します！");
-
+		System.out.println("=== Todoリスト ===");
+		
+		
 		Scanner scanner = new Scanner(System.in);
 		ArrayList<String> todoList = new ArrayList<>();
 			
 		
 		while(true){
-			System.out.print("タスクを入力してください：");
-			String task = scanner.nextLine();
-
-			System.out.println("「" + task + "」を登録しました");
-			if(task.equals("exit")){
-				break;
+			System.out.println("\n--- 現在のタスク ---");
+			if(todoList.size() == 0){
+				System.out.println("(なし)");
+			}else{
+				for(int i = 0; i < todoList.size(); i++){
+					System.out.println((i + 1) + "." + todoList.get(i));
+				}
 			}
-			todoList.add(task);
-			System.out.println("現在のタスク：" + todoList);
+			System.out.println("----------------------");
+
+			System.out.println("タスクを入力（削除なら'del',終了なら'exit')");
+			String input = scanner.nextLine();
+
+			if(input.equals("exit")){
+				break;
+			}else if (input.equals("del")){
+				System.out.println("削除する番号を入力してくださ：");
+				String numStr = scanner.nextLine();
+				int num = Integer.parseInt(numStr);
+
+				todoList.remove(num - 1);
+				System.out.println(">>削除しました");
+			}else{
+				todoList.add(input);
+				System.out.println(">>追加しました");
+			}
+
 
 		}
 	System.out.println("アプリを終了します。");
 
 	}
 }
-
