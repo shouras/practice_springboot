b7703016dcd935f4ee99fc59b5986346bcf9b5c0
shouras <h.tomohiro1219@icloud.com>
2026-01-05 18:42:51 +0900

    webでタスクを登録できるフォームを作った

--- src/main/java/com/example/practice_springboot/Controller/TodoController.java
+++ src/main/java/com/example/practice_springboot/Controller/TodoController.java
@@ -2,11 +2,11 @@ package com.example.practice_springboot.Controller;
 
 import java.util.ArrayList;
 
+import org.springframework.stereotype.Controller; // ここが変わりました
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+@Controller
 public class TodoController {
 
     // コンソール版と同じ！タスクを保存する箱
@@ -15,8 +15,9 @@ public class TodoController {
     // 1. リストの中身を見る機能
     // URL: http://localhost:8080/list
     @GetMapping(value = "/list", produces = "application/json; charset=UTF-8")
-    public ArrayList<String> getList() {
-        return todoList;
+    public String getList(Model model) {
+        model.addAttribute("todoList", todoList);
+        return "todo";
     }
 
     // 2. タスクを追加する機能
@@ -24,6 +25,6 @@ public class TodoController {
     @GetMapping("/add")
     public String addTask(@RequestParam("task") String task) {
         todoList.add(task);
-        return "「" + task + "」をリストに追加しました！";
+        return "redirect:/list";
     }
 }
